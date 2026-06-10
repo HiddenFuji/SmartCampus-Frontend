002a96e724a3f1db6ca5cce41d0d2ce7ab7dc70a
Azib Hariz <masterlizard123@gmail.com>
2026-06-11 02:55:08 +0800

    Changes to BaseURL for Deployment

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://smartcampus-backend-a6g3.onrender.com",
 });
 
 export default api;
